757a9f1a0cffc918a0621d43c3e934589af813c2
lxq <dearbosslee@gmail.com>
2021-11-04 11:49:27 +0800

    【test】test04

--- test/README.md
+++ test/README.md
@@ -8,4 +8,5 @@
 | test01 | 测试 std::vector 中元素是保存在栈上还是堆上 |
 | test02 | 测试空类的大小 |
 | test03 | 测试 std::ifstream 读写文件 |
+| test04 | 测试 shared_ptr 作为成员变量时的析构顺序 |
 
---
+++ test/test04_shared_ptr.cpp
@@ -0,0 +1,80 @@
+//
+// Created by lixiaoqing on 2021/11/4.
+//
+
+#include <iostream>
+#include <memory>
+using namespace std;
+
+/**
+运行结果：
+>>> test01 start.
+Person::Person()
+A::A()
+B::B()
+>>> After construct p1,p2.
+>>> After make shared.
+Person::~Person()
+B::~B()
+A::~A()
+>>> test01 end.
+
+结论：
+变量构造的顺序是：Person --- A --- B ，成员变量 A 和 B 构造时按照声明的顺序构造。
+变量析构的顺序是：Person --- B --- A ，成员变量 A 和 B 析构时析构的顺序与声明的顺序相反。
+ */
+
+
+class A {
+public:
+    A() {
+        cout << __PRETTY_FUNCTION__ << endl;
+    }
+
+    ~A() {
+        cout << __PRETTY_FUNCTION__ << endl;
+    }
+};
+
+class B {
+public:
+    B() {
+        cout << __PRETTY_FUNCTION__ << endl;
+    }
+
+    ~B() {
+        cout << __PRETTY_FUNCTION__ << endl;
+    }
+};
+
+class Person {
+public:
+    Person() {
+        cout << __PRETTY_FUNCTION__ << endl;
+        p1 = make_shared<A>();
+        p2 = make_shared<B>();
+        cout << ">>> After construct p1,p2." << endl;
+    }
+
+    ~Person() {
+        cout << __PRETTY_FUNCTION__ << endl;
+    }
+
+private:
+    shared_ptr<A> p1;
+    shared_ptr<B> p2;
+};
+
+void test01() {
+    cout << ">>> test01 start." << endl;
+    {
+        auto p = make_shared<Person>();
+        cout << ">>> After make shared." << endl;
+    }
+    cout << ">>> test01 end." << endl;
+}
+
+int main() {
+    test01();
+    return 0;
+}
