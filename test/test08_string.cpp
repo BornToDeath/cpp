f7190e073ebdfe598d1bf02f7627f8a09b458588
lxq <dearbosslee@gmail.com>
2021-12-08 21:06:54 +0800

    [test] test08

--- test/test08_string.cpp
+++ test/test08_string.cpp
@@ -16,34 +16,40 @@
 unsigned long long getCurrentTimeMills();
 
 void test01();
+
 void test02();
+
+void test03();
+void test04();
 
 
 int main() {
-    auto t1 = getCurrentTimeMills();
-    test01();
-    auto t2 = getCurrentTimeMills();
-    std::cout << "test01 耗时：" << (t2-t1) << std::endl;
+//    auto t1 = getCurrentTimeMills();
+//    test01();
+//    auto t2 = getCurrentTimeMills();
+//    std::cout << "test01 耗时：" << (t2-t1) << std::endl;
+//
+//    t1 = getCurrentTimeMills();
+//    test02();
+//    t2 = getCurrentTimeMills();
+//    std::cout << "test02 耗时：" << (t2-t1) << std::endl;
 
-    t1 = getCurrentTimeMills();
-    test02();
-    t2 = getCurrentTimeMills();
-    std::cout << "test02 耗时：" << (t2-t1) << std::endl;
-
+    test04();
     return 0;
 }
 
 void test01() {
     std::string str("");
-    for(int i=0;i<100000;++i) {
+    for (int i = 0; i < 100000; ++i) {
         str = "mei xiang ni yi ci, tian shang jiu piao luo yi li sha, yu shi bian you le sa ha la. mei xiang ni yi ci, tian shang jiu luo xia yi di shui, yu shi bian you le tai ping yang.";
         std::string aaa = str;
     }
 }
 
 void test02() {
-    for(int i=0;i<100000;++i) {
-        std::string str("mei xiang ni yi ci, tian shang jiu piao luo yi li sha, yu shi bian you le sa ha la. mei xiang ni yi ci, tian shang jiu luo xia yi di shui, yu shi bian you le tai ping yang.");
+    for (int i = 0; i < 100000; ++i) {
+        std::string str(
+                "mei xiang ni yi ci, tian shang jiu piao luo yi li sha, yu shi bian you le sa ha la. mei xiang ni yi ci, tian shang jiu luo xia yi di shui, yu shi bian you le tai ping yang.");
         std::string aaa = str;
     }
 }
@@ -54,4 +60,25 @@ unsigned long long getCurrentTimeMills() {
             std::chrono::steady_clock::now().time_since_epoch()
     );
     return us.count();
+}
+
+void test03() {
+    std::cout << std::boolalpha;
+    std::string s1 = "100000";
+    std::string s2 = "100001";
+    std::cout << s1.compare(s2) << std::endl;
+    std::cout << (s1 < s2) << std::endl;
+}
+
+void test04() {
+    class A {
+    public:
+        std::string s;
+    };
+
+    A a;
+    std::cout << sizeof(a) << std::endl;
+
+    a.s.append("123");
+    std::cout << sizeof(a) << std::endl;
 }
