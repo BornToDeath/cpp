d395272ccc0fb819d6749c0dc195a5312c64652a
lxq <dearbosslee@gmail.com>
2022-05-07 20:49:06 +0800

    [刷题] 判断链表中是否有环

---
+++ 刷题/牛客题霸/BM6_判断链表中是否有环.cpp
@@ -0,0 +1,25 @@
+//
+// Created by lixiaoqing on 2022/5/7.
+//
+
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        auto slow = head, fast = head;
+        while (fast && fast->next) {
+            fast = fast->next->next;
+            slow = slow->next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
