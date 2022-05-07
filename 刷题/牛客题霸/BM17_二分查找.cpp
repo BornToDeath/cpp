e0858d240f6af7a6fe328fc78d6ee06d0b67f71d
lxq <dearbosslee@gmail.com>
2022-05-07 21:43:28 +0800

    [刷题] 二分查找

---
+++ 刷题/牛客题霸/BM17_二分查找.cpp
@@ -0,0 +1,34 @@
+//
+// Created by lixiaoqing on 2022/5/7.
+//
+
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    /**
+     * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+     *
+     *
+     * @param nums int整型vector
+     * @param target int整型
+     * @return int整型
+     */
+    int search(vector<int> &nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+        while (left <= right) {
+            auto mid = (right - left) / 2 + left;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
+};
