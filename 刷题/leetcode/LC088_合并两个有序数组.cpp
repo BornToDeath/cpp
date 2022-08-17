7b6bbb354f92b233d67730a1c408fbeaf9a44d71
lxq <dearbosslee@gmail.com>
2022-08-17 11:34:00 +0800

    [刷题] LC88: 合并两个有序数组

---
+++ 刷题/leetcode/LC088_合并两个有序数组.cpp
@@ -0,0 +1,26 @@
+//
+// Created by lixiaoqing on 2022/8/17.
+//
+
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    void merge(vector<int> &nums1, int m, vector<int> &nums2, int n) {
+        while (m > 0 && n > 0) {
+            if (nums1[m - 1] >= num2[n - 1]) {
+                nums1[m + n - 1] = nums1[m - 1];
+                --m;
+            } else {
+                nums1[m + n - 1] = nums2[n - 1];
+                --n;
+            }
+        }
+        while (n > 0) {
+            nums1[m + n - 1] = nums2[n - 1];
+            --n;
+        }
+    }
+};
