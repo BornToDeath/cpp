42e5f01619dbb7f158990ad6c160f47c756d2dc2
lxq <dearbosslee@gmail.com>
2022-10-25 17:51:28 +0800

    [test] 无锁队列之 RingBuffer

---
+++ test/case14_无锁队列/RingBuffer.h
@@ -0,0 +1,75 @@
+//
+// Created by lixiaoqing on 2022/10/25.
+//
+
+/**
+ * 无锁环形缓冲区
+ * @tparam T
+ */
+template<typename T>
+class RingBuffer {
+public:
+    explicit RingBuffer(unsigned int size) : m_size(size + 1), m_head(0), m_tail(0) {
+        m_data = new T[m_size]();
+    }
+
+    ~RingBuffer() {
+        delete[] m_data;
+        m_data = nullptr;
+    }
+
+public:
+    bool push(const T &value) {
+        if (isFull()) {
+//            printf("head=%d, tail=%d, value=%d\n", m_head, m_tail, value);
+            return false;
+        }
+        m_data[m_tail] = value;
+        m_tail = (m_tail + 1) % m_size;
+        return true;
+    }
+
+    T pop() {
+        if (isEmpty()) {
+            throw "ring buffer is empty";
+        }
+        auto value = m_data[m_head];
+        m_head = (m_head + 1) % m_size;
+        return value;
+    }
+
+    bool isFull() const {
+        return m_head == (m_tail + 1) % m_size;
+    }
+
+    bool isEmpty() const {
+        return m_head == m_tail;
+    }
+
+    T *head() const {
+        if (isEmpty()) {
+            return nullptr;
+        }
+        return m_data + m_head;
+    }
+
+    T *tail() const {
+        if (isEmpty()) {
+            return nullptr;
+        }
+        if (m_tail == 0) {
+            return m_data + m_size - 1;
+        }
+        return m_data + m_tail - 1;
+    }
+
+    unsigned int capacity() const {
+        return m_size;
+    }
+
+private:
+    const unsigned int m_size;  // 队列长度
+    int m_head;                 // 队列头部索引
+    int m_tail;                 // 队列尾部索引
+    T *m_data;                  // 数据缓冲区
+};
