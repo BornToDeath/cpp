0a466b4996c782a1239e04f495036489f08fc608
lxq <dearbosslee@gmail.com>
2022-10-26 16:33:58 +0800

    [test] LockFreeQueue

--- test/case14_无锁队列/LockFreeQueue.h
+++ test/case14_无锁队列/LockFreeQueue.h
@@ -11,11 +11,11 @@
 //#include <sys/types.h>
 #include <fcntl.h>
 
-#define USE_LOCK  // 开启 spinlock 锁，多生产者多消费者场景
-#define USE_POT   // 开启队列大小的2的幂对齐
-#define USE_MB    // 开启 memory barrier
-#define IS_POT(x) ((x) && !((x) & ((x) - 1)))
-#define MEMORY_BARRIER __sync_synchronize()
+#define USE_LOCK  // 开启 spinlock 锁，适用多生产者多消费者场景
+//#define USE_POT  // 开启队列大小的2的幂对齐。目的是方便计算头尾索引
+//#define USE_MEM_BAR  // 开启 memory barrier
+#define IS_POT(x) ((x) && !((x) & ((x) - 1)))  // 判断 x 是否是 2 的幂
+#define MEMORY_BARRIER __sync_synchronize()  // 开启内存屏障
 
 /**
  * 无锁队列
@@ -27,7 +27,7 @@ class LockFreeQueue {
 
 public:
 
-    explicit LockFreeQueue(unsigned int size, const char *name = nullptr) : m_head(0), m_tail(0) {
+    explicit LockFreeQueue(unsigned int size, const char *name = nullptr) {
 
 #ifdef USE_POT
         if (!IS_POT(size)) {
@@ -39,16 +39,19 @@ public:
         if (name == nullptr) {
             m_buffer = new T[m_size]();
         } else {
+            // 创建共享内存信道
             int shm_fd = shm_open(name, O_CREAT | O_RDWR, 0666);
             if (shm_fd < 0) {
                 perror("shm_open failed");
                 exit(1);
             }
+            // 设置共享内存的大小
             if (ftruncate(shm_fd, m_size * sizeof(T)) < 0) {
                 perror("ftruncate failed");
                 close(shm_fd);
                 exit(1);
             }
+            // 共享内存映射
             void *addr = mmap(nullptr, m_size * sizeof(T), PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
             if (addr == MAP_FAILED) {
                 perror("mmap failed");
@@ -65,10 +68,9 @@ public:
     }
 
     ~LockFreeQueue() {
-        if (shm_name[0] == 0) {
+        if (shm_name[0] == 0) {  // 创建的是堆内存
             delete[] m_buffer;
-            m_buffer = nullptr;
-        } else {
+        } else {  // 创建的是共享内存
             if (munmap(m_buffer, m_size * sizeof(T)) == -1) {
                 perror("munmap failed");
             }
@@ -76,24 +78,25 @@ public:
                 perror("shm_unlink failed");
             }
         }
+        m_buffer = nullptr;
     }
 
 public:
 
     bool push(const T &value) {
 #ifdef USE_LOCK
-        m_lock.spinlock_lock();
+        m_lock.lock();
 #endif
         if (isFull()) {
 #ifdef USE_LOCK
-            m_lock.spinlock_unlock();
+            m_lock.unlock();
 #endif
             return false;
         }
 
         memcpy(m_buffer + m_tail, &value, sizeof(T));
 
-#ifdef USE_MB
+#ifdef USE_MEM_BAR
         MEMORY_BARRIER;
 #endif
 
@@ -104,7 +107,7 @@ public:
 #endif
 
 #ifdef USE_LOCK
-        m_lock.spinlock_unlock();
+        m_lock.unlock();
 #endif
 
         return true;
@@ -112,18 +115,18 @@ public:
 
     bool pop(T &value) {
 #ifdef USE_LOCK
-        m_lock.spinlock_lock();
+        m_lock.lock();
 #endif
         if (isEmpty()) {
 #ifdef USE_LOCK
-            m_lock.spinlock_unlock();
+            m_lock.unlock();
 #endif
             return false;
         }
 
         memcpy(&value, m_buffer + m_head, sizeof(T));
 
-#ifdef USE_MB
+#ifdef USE_MEM_BAR
         MEMORY_BARRIER;
 #endif
 
@@ -134,7 +137,7 @@ public:
 #endif
 
 #ifdef USE_LOCK
-        m_lock.spinlock_unlock();
+        m_lock.unlock();
 #endif
         return true;
     }
@@ -159,8 +162,13 @@ public:
         return m_tail;
     }
 
-protected:
+private:
 
+    /**
+     * 计算大于 size 的第一个 2 的幂。如 size = 10, 则返回 16
+     * @param size
+     * @return
+     */
     unsigned int roundup_pow_of_two(size_t size) {
         size |= size >> 1;
         size |= size >> 2;
@@ -171,14 +179,14 @@ protected:
         return size + 1;
     }
 
-protected:
-    class spinlock_t {
+private:
+    class spinlock {
     public:
-        void spinlock_lock() {
+        void lock() {
             while (!__sync_bool_compare_and_swap(&m_lock, 0, 1)) {}
         }
 
-        void spinlock_unlock() {
+        void unlock() {
             __sync_lock_release(&m_lock);
         }
 
@@ -186,14 +194,14 @@ protected:
         int m_lock{0};
     };
 
-protected:
-    char shm_name[32]{};           // 共享内存 key 的路径名称
-    volatile unsigned int m_head;  // 队列头部索引
-    volatile unsigned int m_tail;  // 队列尾部索引（指向队列尾部元素的下一个元素）
-    unsigned int m_size;           // 队列大小
-    T *m_buffer;
+private:
+    char shm_name[32]{};              // 共享内存 key 的路径名称
+    volatile unsigned int m_head{0};  // 队列头部索引
+    volatile unsigned int m_tail{0};  // 队列尾部索引（指向队列尾部元素的下一个元素）
+    unsigned int m_size{0};           // 队列大小
+    T *m_buffer{nullptr};             // 数据缓冲区
 #ifdef USE_LOCK
-    spinlock_t m_lock;
+    spinlock m_lock;                  // CAS 实现的锁机制
 #endif
 };
 
