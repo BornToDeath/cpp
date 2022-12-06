2ae57902ca73b0499c2f8c22da6b1f11026854f6
lxq <dearbosslee@gmail.com>
2022-12-06 21:24:13 +0800

    [算法] 快速排序之非递归写法

--- 排序算法/031_快速排序.cpp
+++ 排序算法/031_快速排序.cpp
@@ -4,6 +4,7 @@
 
 #include <iostream>
 #include <vector>
+#include <stack>
 
 inline void printArray(const std::vector<int> &nums) {
     std::for_each(nums.begin(), nums.end(), [](int num) { std::cout << num << ", "; });
@@ -39,12 +40,56 @@ void quick_sort(std::vector<int> &nums, int left, int right) {
     quick_sort(nums, i + 1, right);
 }
 
+int quick_sort2(std::vector<int> &arr, int start, int end) {
+    if (start >= end) {
+        return start;
+    }
+    int base = arr[start];
+    int i = start, j = end;
+    while (i < j) {
+        while (i < j && arr[j] > base) {
+            --j;
+        }
+        while (i < j && arr[i] <= base) {
+            ++i;
+        }
+        if (i < j) {
+            std::swap(arr[i], arr[j]);
+        }
+    }
+    std::swap(arr[start], arr[i]);
+    return i;
+}
+
 /**
  * 快速排序，从小到大
  * @param nums
  */
 void quick_sort(std::vector<int> &nums) {
-    quick_sort(nums, 0, nums.size() - 1);
+    /**
+     * 方法1：递归写法
+     */
+//    quick_sort(nums, 0, nums.size() - 1);
+
+    /**
+     * 方法2：非递归写法
+     */
+    if (nums.empty()) {
+        return;
+    }
+    std::stack<std::pair<int, int>> _stack;
+    _stack.push({0, nums.size() - 1});
+    while (!_stack.empty()) {
+        auto interval = _stack.top();
+        _stack.pop();
+        int pos = quick_sort2(nums, interval.first, interval.second);
+        if (pos > interval.first) {
+            _stack.push({interval.first, pos - 1});
+        }
+        if (pos < interval.second) {
+            _stack.push({pos + 1, interval.second});
+        }
+    }
 }
 
 int main() {
