4aaf43e7cd12aa678c41f17d85d94200f63e68d4
lxq <dearbosslee@gmail.com>
2023-01-11 10:52:00 +0800

    [刷题] LC100: 相同的树

--- 刷题/leetcode/LC100_相同的树.cpp
+++ 刷题/leetcode/LC100_相同的树.cpp
@@ -14,20 +14,53 @@
  * };
  */
 
+#include <queue>
 #include "TreeNode.h"
 
 class Solution {
 public:
     bool isSameTree(TreeNode *p, TreeNode *q) {
-        if (p == nullptr || q == nullptr) {
-            if (p == nullptr && q == nullptr) {
-                return true;
+        /**
+         * 方法1：递归（DFS）
+         */
+//        if (p == nullptr || q == nullptr) {
+//            if (p == nullptr && q == nullptr) {
+//                return true;
+//            }
+//            return false;
+//        }
+//        if (p->val != q->val) {
+//            return false;
+//        }
+//        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+
+        /**
+         * 方法2：迭代（BFS）
+         */
+        std::queue<TreeNode *> q1, q2;
+        q1.push(p);
+        q2.push(q);
+        while (!q1.empty() && !q2.empty()) {
+            auto node1 = q1.front();
+            q1.pop();
+            auto node2 = q2.front();
+            q2.pop();
+            if (node1 == nullptr || node2 == nullptr) {
+                if (node1 == nullptr && node2 == nullptr) {
+                    continue;
+                }
+                return false;
+            } else if (node1->val != node2->val) {
+                return false;
             }
-            return false;
+            q1.push(node1->left);
+            q1.push(node1->right);
+            q2.push(node2->left);
+            q2.push(node2->right);
         }
-        if (p->val != q->val) {
-            return false;
+        if (q1.empty() && q2.empty()) {
+            return true;
         }
-        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+        return false;
     }
 };
