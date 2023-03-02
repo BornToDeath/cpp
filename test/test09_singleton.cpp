3b254a10e2b9719eb139679a5d200b7f45d69d38
lxq <dearbosslee@gmail.com>
2023-03-02 21:02:02 +0800

    [test] singleton test

--- test/test09_singleton.cpp
+++ test/test09_singleton.cpp
@@ -81,8 +81,39 @@ namespace SingleTest02 {
     }
 }
 
-int main() {
+namespace singleton_test03 {
+    class Singleton {
+    public:
+        static Singleton *&getInstance() {
+            if (instance == nullptr) {
+                std::unique_lock<std::mutex> lock(_mutex);
+                if (instance == nullptr) {
+                    auto obj = new Singleton();
+                    instance = obj;
+                    std::cout << ">>> " << instance << std::endl;
+                }
+            }
+            return instance;
+        }
 
+        static void releaseInstance() {
+            if (instance) {
+                std::unique_lock<std::mutex> lock(_mutex);
+                delete instance;
+                instance = nullptr;
+            }
+        }
+
+    private:
+        static Singleton *instance;
+        static std::mutex _mutex;
+    };
+
+    Singleton *Singleton::instance = nullptr;
+    std::mutex Singleton::_mutex;
+}
+
+void test01() {
     auto runnable = [] {
         auto ptr = SingleTest02::Singleton::getSingleton();
         ptr->print();
@@ -95,6 +126,28 @@ int main() {
     t1.join();
     t2.join();
     t3.join();
+}
 
+void test02() {
+    using singleton_test03::Singleton;
+
+    auto instance = Singleton::getInstance();
+    std::cout << "1: " << instance << std::endl;
+
+    instance = nullptr;
+    std::cout << instance << std::endl;
+
+    std::cout << "2: " << Singleton::getInstance() << std::endl;
+
+    Singleton::getInstance() = nullptr;   // 如果单例返回的是指针的引用，则此处单例实例被修改为了空！有风险！造成内存泄露
+
+    std::cout << "3: " << Singleton::getInstance() << std::endl;  // 此处会重新创建一份单例实例，
+
+    Singleton::releaseInstance();
+}
+
+int main() {
+    test02();
     std::cout << "Exit." << std::endl;
+    return 0;
 }
