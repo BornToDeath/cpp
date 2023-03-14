5146c97be47407626a108782817a9df0dbcdcbf9
lxq <dearbosslee@gmail.com>
2023-03-14 11:11:23 +0800

    [algo] JZ25: 合并两个排序的链表

---
+++ algorithms/jzoffer2/JZ25_合并两个排序的链表.cpp
@@ -0,0 +1,50 @@
+//
+// Created by lixiaoqing on 2023/3/14.
+//
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+#include "ListNode.h"
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if (l1 == nullptr) {
+            return l2;
+        } else if (l2 == nullptr) {
+            return l1;
+        }
+        ListNode *head = nullptr, *cur = nullptr;
+        if (l1->val < l2->val) {
+            head = cur = l1;
+            l1 = l1->next;
+        } else {
+            head = cur = l2;
+            l2 = l2->next;
+        }
+        while (l1 && l2) {
+            if (l1->val < l2->val) {
+                cur->next = l1;
+                l1 = l1->next;
+            } else {
+                cur->next = l2;
+                l2 = l2->next;
+            }
+            cur = cur->next;
+        }
+        if (l1) {
+            cur->next = l1;
+        }
+        if (l2) {
+            cur->next = l2;
+        }
+        return head;
+    }
+};
