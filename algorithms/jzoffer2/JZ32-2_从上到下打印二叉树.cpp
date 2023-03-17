6b3b3140b46bd61652807aa8c6b7862dca681196
lxq <dearbosslee@gmail.com>
2023-03-17 16:36:28 +0800

    [algo] JZ32-2:从上到下打印二叉树

---
+++ algorithms/jzoffer2/JZ32-2_从上到下打印二叉树.cpp
@@ -0,0 +1,47 @@
+//
+// Created by lixiaoqing on 2023/3/17.
+//
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+#include <vector>
+#include <queue>
+#include "TreeNode.h"
+
+using std::vector;
+
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode *root) {
+        if (root == nullptr) {
+            return {};
+        }
+        std::vector<std::vector<int>> ans;
+        std::queue<TreeNode *> q;
+        q.push(root);
+        while (!q.empty()) {
+            auto size = q.size();
+            ans.push_back(std::vector<int>(size));
+            for (int i = 0; i < size; ++i) {
+                auto node = q.front();
+                q.pop();
+                ans.back()[i] = node->val;
+                if (node->left) {
+                    q.push(node->left);
+                }
+                if (node->right) {
+                    q.push(node->right);
+                }
+            }
+        }
+        return ans;
+    }
+};
