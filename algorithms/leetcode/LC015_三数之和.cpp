edf05e19d0788ff974c84eb9ba7abef9c39fc7b9
lxq <dearbosslee@gmail.com>
2023-08-17 09:52:55 +0800

    [algo] LC015_三数之和

--- algorithms/leetcode/LC015_三数之和.cpp
+++ algorithms/leetcode/LC015_三数之和.cpp
@@ -51,36 +51,78 @@ public:
         /**
          * 方法2：时间复杂度O(nlogn+n^2)，即O(n^2)
          */
-        std::vector<std::vector<int>> res;
+//        std::vector<std::vector<int>> res;
+//        std::sort(nums.begin(), nums.end());
+//        int len = nums.size();
+//        for (int i = 0; i < len - 1; ++i) {
+//            // 第一个数需要和上一次的不同
+//            if (i > 0 && nums[i] == nums[i - 1]) {
+//                continue;
+//            }
+//            for (int j = i + 1, k = len - 1; j < k; ++j) {
+//                // 第二个数需要和上一次的不同
+//                if (j > i + 1 && nums[j] == nums[j - 1]) {
+//                    continue;
+//                }
+//                while (j < k && nums[i] + nums[j] + nums[k] > 0) {
+//                    --k;
+//                }
+//                if (j >= k) {
+//                    break;
+//                }
+//                if (nums[i] + nums[j] + nums[k] == 0) {
+//                    res.push_back({nums[i], nums[j], nums[k]});
+//                }
+//            }
+//        }
+//        return res;
+
+        /*
+         * 方法3: 排序 + 双指针 (同方法2, 但是易于理解)
+         * 时间复杂度 O(nlogn + n^2 = n^2), 空间复杂度 O(logn) (即快排的空间复杂度)
+         */
+        std::vector<std::vector<int>> ans;
+        const int n = nums.size();
+        if (n < 3) {
+            return ans;
+        }
         std::sort(nums.begin(), nums.end());
-        int len = nums.size();
-        for (int i = 0; i < len - 1; ++i) {
-            // 第一个数需要和上一次的不同
+        for (int i = 0; i < n; ++i) {
+            // 如果第一个数都大于0, 则和不可能为0
+            if (nums[i] > 0) {
+                break;
+            }
+            // 第一个数需要和上一次的数不相同
             if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
-            for (int j = i + 1, k = len - 1; j < k; ++j) {
-                // 第二个数需要和上一次的不同
-                if (j > i + 1 && nums[j] == nums[j - 1]) {
-                    continue;
-                }
-                while (j < k && nums[i] + nums[j] + nums[k] > 0) {
+            for (int j = i + 1, k = n - 1; j < k;) {
+                if (nums[i] + nums[j] + nums[k] < 0) {
+                    ++j;
+                } else if (nums[i] + nums[j] + nums[k] > 0) {
                     --k;
-                }
-                if (j >= k) {
-                    break;
-                }
-                if (nums[i] + nums[j] + nums[k] == 0) {
-                    res.push_back({nums[i], nums[j], nums[k]});
+                } else {
+                    ans.emplace_back(std::vector<int>{nums[i], nums[j], nums[k]});
+                    ++j;
+                    --k;
+                    // 第二个数不重复选取
+                    while (j < k && nums[j] == nums[j - 1]) {
+                        ++j;
+                    }
+                    // 第三个数不重复选取
+                    while (j < k && nums[k] == nums[k + 1]) {
+                        --k;
+                    }
                 }
             }
         }
-        return res;
+        return ans;
     }
 };
 
 int main() {
     std::vector<int> nums = {-1, 0, 1, 2, -1, -4};
+    nums = {0, 0, 0, 0};
     auto res = Solution().threeSum(nums);
     print(res);
     return 0;
