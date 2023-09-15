3fdeb5ea70bcf4a0331a0dc17909b2998efd0ab5
lxq <dearbosslee@gmail.com>
2023-09-15 18:09:54 +0800

    [test] test googletest samples

--- test/case06_gtest/test.cpp
+++ test/case06_gtest/test.cpp
@@ -4,28 +4,7 @@
 
 #include "gtest/gtest.h"
 
-//namespace Test {
-//    void test01() {
-//        auto add = [](int a, int b) {
-//            return a + b;
-//        };
-//    }
-//}
-//
-//int add(int a, int b) {
-//    return a + b;
-//};
-//
-//TEST(Test, test01) {
-//    EXPECT_EQ(add(4, 3), 7);
-//}
-
 int main() {
-//    int num = 1;
-//    printf(">>> %d\n", (num++) + (num++));  // 3
-//    printf("num=%d\n", num);                // 3
-//    printf(">>> %d\n", (++num) + (++num));  // 9
-//    printf("num=%d\n", num);                // 5
-    testing::InitGoogleTest();
+    ::testing::InitGoogleTest();
     return RUN_ALL_TESTS();
 }
